2a4a81987d6793862b70ad404f66b64dc95cd512
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-11 14:53:56 +0530

    Added UC1 Address Book Created

---
+++ AddressBookProblem/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class Address
+    {
+        public string firstName, firstName2, lastName, lastName2, address, address2, city, city2, state, state2, zip, zip2, phoneNumber, phoneNumber2, email, email2;
+        public int value;
+    }
+}
